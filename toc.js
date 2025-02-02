7607bc6f688272c7126d7ae6f60c2bd55b4d0f3d
Cody-Duncan <Cody-Duncan@users.noreply.github.com>
2025-02-02 10:44:42 +0000

    deploy: cab7fc9dead69daf01327e5886e3b8de0257ccdf

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Chapter 1</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="intro.html">r/GraphicsProgramming Wiki</a></li><li class="chapter-item expanded "><a href="start_here.html"><strong aria-hidden="true">1.</strong> Want to get started in Graphics Programming? Start Here!</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
